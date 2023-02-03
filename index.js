a6bcd190bae7ba32e51d1fd97b1d8d0f0fa3734c
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-02-04 00:02:39 +0100

    add post, user

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { 
+const {
   MONGO_USER,
   MONGO_PASSWORD,
   MONGO_IP,
   MONGO_PORT,
 } = require("./config/config");
+const postRouter = require("./routes/postRoutes");
+const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 mongoose.set("strictQuery", false);
@@ -19,7 +21,7 @@ const connectWithRetry = () => {
       useUnifiedTopology: true,
     })
     .then(() => console.log("succesfully connected to DB"))
-    .catch((e) => { 
+    .catch((e) => {
       console.log(e);
       console.log("retry connecting every 5 seconds...");
       setTimeout(connectWithRetry, 5000);
@@ -28,12 +30,20 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+// add middleware
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi there!</h2>");
 });
 
+
+// user routes
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRoutes);
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`listining on port ${port}`));
+app.listen(port, () => {
+  console.log(`Your server is running at port number: ${port}`);
+});
