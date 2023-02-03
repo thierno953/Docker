d153aa0409dbbfef721ef51983fa2650deebc777
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-02-03 22:59:48 +0100

    add mongo

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 const app = express();
+
+mongoose.set("strictQuery", false);
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("succesfully connected to DB"))
+    .catch((e) => { 
+      console.log(e);
+      console.log("retry connecting every 5 seconds...");
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.use(express.json());
 
